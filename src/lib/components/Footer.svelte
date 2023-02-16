7e820a4ec36ce14ad901a45314aba1a6cf779469
Zach Blake <ztb@pm.me>
2023-02-15 22:23:29 -0800

    footer

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,13 @@
+<script>
+	import WithLove from './WithLove.svelte';
+</script>
+
+<footer>
+	<WithLove />
+</footer>
+
+<style>
+	footer {
+		max-width: 100%;
+	}
+</style>
---
+++ src/lib/components/WithLove.svelte
@@ -0,0 +1,19 @@
+<p>
+	Built with
+	<span> 🫀 </span>
+	by
+	<a href="https://ztb.dev">Zach Blake</a>
+</p>
+
+<style>
+	p {
+		text-align: center;
+	}
+
+	a,
+	a:visited,
+	a:hover,
+	a:active {
+		color: #48a6e4;
+	}
+</style>
