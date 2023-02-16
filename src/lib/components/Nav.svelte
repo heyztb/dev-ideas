5925c4e2436e59ee734ebc0a66754f2cfda891aa
Zach Blake <ztb@pm.me>
2023-02-15 19:13:07 -0800

    layout things

---
+++ src/lib/components/Nav.svelte
@@ -0,0 +1,11 @@
+<nav>
+  <a href="/">Home</a>
+  <a href="/about">About</a>
+  <a href="/signup">Signup</a>
+</nav>
+
+<style>
+  nav > a:not(:first-child) {
+    margin-left: 1rem;
+  }
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import Nav from "../lib/components/Nav.svelte";
+</script>
+
+<Nav />
+
+<slot></slot>
+
+<style>
+  :global(body) {
+    font-family: Arial, Helvetica, sans-serif;
+  }
+</style>
