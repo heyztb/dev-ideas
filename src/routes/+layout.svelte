3efe272a1af272f0af804281683c03cfa4534a84
Zach Blake <ztb@pm.me>
2023-02-15 22:23:14 -0800

    shamelessly taking these styles from wip.chat

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,26 @@
 <script>
-  import Nav from "../lib/components/Nav.svelte";
+	import Nav from '../lib/components/Nav.svelte';
+	import Footer from '../lib/components/Footer.svelte';
 </script>
 
 <Nav />
 
-<slot></slot>
+<slot />
+
+<Footer />
 
 <style>
-  :global(body) {
-    font-family: Arial, Helvetica, sans-serif;
-  }
+	:global(body) {
+		-webkit-text-size-adjust: 100%;
+		font-feature-settings: normal;
+		font-family: Inter var, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI,
+			Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji,
+			Segoe UI Symbol, Noto Color Emoji;
+		line-height: 1.5;
+		-moz-tab-size: 4;
+		tab-size: 4;
+	}
+	:global(::selection) {
+		background: #ffea00;
+	}
 </style>
