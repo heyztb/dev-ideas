cc32e05266cdfade47b0ad6bf6fc9db32106f170
Zach Blake <ztb@pm.me>
2023-02-15 23:25:19 -0800

    use a proper css file for global styles

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script>
 	import Nav from '../lib/components/Nav.svelte';
 	import Footer from '../lib/components/Footer.svelte';
+	import '../../static/global.css';
 </script>
 
 <Nav />
@@ -8,19 +9,3 @@
 <slot />
 
 <Footer />
-
-<style>
-	:global(body) {
-		-webkit-text-size-adjust: 100%;
-		font-feature-settings: normal;
-		font-family: Inter var, ui-sans-serif, system-ui, -apple-system, BlinkMacSystemFont, Segoe UI,
-			Roboto, Helvetica Neue, Arial, Noto Sans, sans-serif, Apple Color Emoji, Segoe UI Emoji,
-			Segoe UI Symbol, Noto Color Emoji;
-		line-height: 1.5;
-		-moz-tab-size: 4;
-		tab-size: 4;
-	}
-	:global(::selection) {
-		background: #ffea00;
-	}
-</style>
