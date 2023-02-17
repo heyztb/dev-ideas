a4bb0fbc9db8a02a0b912fdf6924bf3e040170b9
Zach Blake <ztb@pm.me>
2023-02-17 14:16:27 -0800

    update hero and footer

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -10,18 +10,15 @@
 	<P class="text-center mb-6 text-lg lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400"
 		>Grow your brand by leveraging automation to share to multiple subreddits, simultaneously.</P
 	>
-	<Button href="#features"
-		>Try it for free
-		<svg
-			class="ml-2 -mr-1 w-5 h-5"
-			fill="currentColor"
-			viewBox="0 0 20 20"
-			xmlns="http://www.w3.org/2000/svg"
-			><path
-				fill-rule="evenodd"
-				d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-				clip-rule="evenodd"
-			/></svg
-		>
-	</Button>
+	<button
+		data-tf-popup="KEmpKAgE"
+		data-tf-opacity="100"
+		data-tf-size="100"
+		data-tf-iframe-props="title=rAuto Early Signup"
+		data-tf-transitive-search-params
+		data-tf-medium="snippet"
+		style="all:unset;font-family:Helvetica,Arial,sans-serif;display:inline-block;max-width:100%;white-space:nowrap;overflow:hidden;text-overflow:ellipsis;background-color:#1a56db;color:#fff;font-size:20px;border-radius:25px;padding:0 33px;font-weight:bold;height:50px;cursor:pointer;line-height:50px;text-align:center;margin:0;text-decoration:none;"
+		>Sign up</button
+	>
+	<script src="//embed.typeform.com/next/embed.js"></script>
 </div>
