7514ded97ee27fff0afe679bc92311f25fa1219f
Zach Blake <ztb@pm.me>
2023-02-20 14:33:02 -0800

    publishing app branch

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -1,12 +1,15 @@
 <script>
-	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger } from 'flowbite-svelte';
+	import { Navbar, NavBrand, NavLi, NavUl, NavHamburger, Button } from 'flowbite-svelte';
 </script>
 
 <Navbar navClass="m-4 sm:m-0" let:hidden let:toggle>
 	<NavBrand href="/">
 		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"> rAuto </span>
 	</NavBrand>
-	<NavHamburger on:click={toggle} />
+	<div class="flex md:order-2">
+		<Button size="sm" href="/signup">Sign up</Button>
+		<NavHamburger on:click={toggle} />
+	</div>
 	<NavUl {hidden}>
 		<NavLi href="#about">About</NavLi>
 		<NavLi href="#features">Features</NavLi>
